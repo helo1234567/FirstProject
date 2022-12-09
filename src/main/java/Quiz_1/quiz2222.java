944cce0aa6b7b75dfcacb95416f50553e29b9235
helo1234567 <asadrazamahmood@gmail.com>
2022-12-09 04:26:33 -0500

    First commit

---
+++ src/main/java/Quiz_1/quiz2222.java
@@ -0,0 +1,7 @@
+package Quiz_1;
+
+public class quiz2222 {
+	
+	
+
+}
---
+++ src/test/java/excelintegration/data.java
@@ -0,0 +1,10 @@
+package excelintegration;
+
+public class data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
